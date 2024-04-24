50ff5aa68e8bb886f113f640eb7d48969f70d0ca
Prit2020 <120326935+Prit2020@users.noreply.github.com>
2024-04-24 14:15:06 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+// Primitive data types
+/*
+
+number
+bigint
+string => "" always write it in double quotes
+boolean => true or flase
+null => it a data type as well as stand alone value
+undefined => value is not defined
+symbol => it is used for uniqueness
+
+*/
+
+// Objects
+/* 
+
+*/
+
+let name = "prit"
+let age = 18
+let state
+
+console.log(typeof null); // it will return object as output
+console.log(typeof undefined); // it will return undefined as output
+console.table([typeof name, typeof age, typeof state])
