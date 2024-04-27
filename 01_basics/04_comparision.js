09760623ec69abec480099d441ca7e08caaf02d3
Prit2020 <120326935+Prit2020@users.noreply.github.com>
2024-04-27 02:51:32 +0000

    boolean comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// basic boolean comparisions
+
+// console.log(2 > 2);  
+// console.log(2 >= 2);
+// console.log(2 == 2);
+// console.log(2 < 2);
+// console.log(2 <= 2);
+// console.log(2 != 2);
+
+// console.log("2" < 1); // the string will be converted into number & then comparision will take place
+
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0); // here null will be converted into 0 & then it will compare
+
+// sometimes null gets converted into NaN and sometimes into 0 & then comparision takes place
+// always compare values of same datatypes
+
+// console.log(undefined > 0);
+// console.log(undefined >= 0); 
+// console.log(undefined == 0);
+
+// === => this is strict check, it will also check the datatype of the given inputs
+console.log("2" === 2); // coz both the datatypes are different 
