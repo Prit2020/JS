cde1fa533ef40e216b3eb4e3ff6ff203361e2162
Prit2020 <120326935+Prit2020@users.noreply.github.com>
2024-05-05 04:07:20 +0000

    datatype-summary

--- 01_basics/dataType_summary.js
+++ 01_basics/dataType_summary.js
@@ -22,17 +22,53 @@ functions
 
 // some eg. of non primitive type
 
-const heros = ["ironman", "hulk", "spiderman"]
+// const heros = ["ironman", "hulk", "spiderman"]
 
-let Obj = 
-{
-    name: "prit",
-    age: 20,
+// let Obj = 
+// {
+//     name: "prit",
+//     age: 20,
+// }
+
+// const myFunction = function() 
+// {
+//    console.log("Hello World"); 
+// }
+
+// console.log(typeof heros);
+
+
+
+// ++++++++++++++++++++
+
+/*
+there are 2 types of memory
+Stack => it is used in primitive data types
+Heap => it is used in non-primitive data types
+
+In stack memory we always get a copy of variable which we have created & changes are made in that copied variable 
+
+In heap memory we get the reference of a object, function,. It means changes which we are making are made in the original object, function
+
+*/
+
+// Eg of stack memory
+// let myYoutubeName = "pritudotcom"
+// let anotherName = myYoutubeName
+// anotherName = "pritchudasama"
+
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+// Eg of heap memory
+let userOne = {
+    emial: "user@google.com",
+    upi: "user@okb"
 }
 
-const myFunction = function() 
-{
-   console.log("Hello World"); 
-}
+let userTwo = userOne
 
-console.log(typeof heros);
+userTwo.emial = "prit@google.com"
+
+console.log(userOne.emial);
+console.log(userTwo.emial);
