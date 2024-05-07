82bd043ddd1e45f5007f8723281b75ad436cfce2
Prit2020 <120326935+Prit2020@users.noreply.github.com>
2024-05-07 14:34:24 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,8 @@
+const name = "prit"  // this is 1st method of declaring a string
+const repoCount = 50
+
+// console.log(name + repoCount + " Value"); => this is old method of concatinating a string
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // This is new way of concatinating a string it is called as string interpolation
+
+const gameName = new String('pritzzz')  // this is 2nd method of declaring string
