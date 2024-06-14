fda8a4aab088b0cce6ebdcd9569dc41769b0feb2
Prit2020 <120326935+Prit2020@users.noreply.github.com>
2024-06-14 06:46:14 +0000

    loops discussions

---
+++ 05_iterations/04_four.js
@@ -0,0 +1,31 @@
+// for in loop
+
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// when we use for in loop for an array then it will return their key instead of values but in for of loop we can directly get the values which are inside an array
+
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
