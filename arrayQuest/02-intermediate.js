66ca619639d084cecedd1a7ceefbcbfe0a91cf11
Prit2020 <120326935+Prit2020@users.noreply.github.com>
2026-01-05 07:48:21 +0000

    02 intermediate started

---
+++ arrayQuest/02-intermediate.js
@@ -0,0 +1,39 @@
+// 1. Implement your own version of map().
+
+// below creating a function which will act like a map() method. Below we define the fun and than we use that function
+// below func takes 2 things (array and callback) func
+function myMapFunction(arr, callback) {
+    const result = []                             // define an empty array here to store the final result, coz map() returns new array
+    for (let i = 0; i < arr.length; i++) {        // iterating an array here
+        result.push(callback(arr[i], i, arr))      // here we access current element, current index, original array
+    }
+    return result
+}
+
+// below we use the above created custom map() method/func
+const arrayOne = [1, 2, 3, 4, 5]
+const newArray = myMapFunction(arrayOne, num => num * num)    // here we invoke myMapFunction and pass (arrayOne, and callback). This is callback func num => num * num
+// console.log(newArray);
+
+
+
+
+// 2. Implement your own version of filter().
+
+// below is the function which acts as a filter() method
+function myFilter(arr, callback) {
+    const result = []
+    for(let i = 0; i < arr.length; i++) {
+        if(callback(arr[i], i, arr)) {        // here if condition will check if the result or value truthy than it will be pushed to new array 
+            result.push(arr[i])
+        }
+    }
+    return result
+}
+
+
+// below is the usage of the myFilter func
+const arrayTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const newArrayTwo = myFilter(arrayTwo, num => num % 2 === 0)
+
+console.log(newArrayTwo);
